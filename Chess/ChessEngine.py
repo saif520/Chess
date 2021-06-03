26dcb7a3665efead5787396df67ba7b167961664
saif520 <62957608+saif520@users.noreply.github.com>
2021-06-03 17:32:06 +0530

    Update ChessEngine.py

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -22,8 +22,14 @@ class GameState():
         self.moveFunctions={'p': self.getPawnMoves, 'R': self.getRookMoves, 'N': self.getKnightMoves,
                             'B': self.getBishopMoves, 'Q': self.getQueenMoves, 'K': self.getKingMoves}
 
-        self.whiteToMove=True
-        self.moveLog=[]
+        self.whiteToMove = True
+        self.moveLog = []
+        self.whiteKingLocation=(7,4)
+        self.blackKingLocation=(0,4)
+        self.checkMate=False
+        self.staleMate=False
+
+
 
     """
     Takes a Move as a parameter and executes it (this will not work for casteling,
@@ -35,19 +41,75 @@ class GameState():
         self.board[move.endRow][move.endCol]=move.pieceMoved
         self.moveLog.append(move)   #log the move so we can undo it letter
         self.whiteToMove=not self.whiteToMove   #swap players
+        #update the king's location if moved
+        if move.pieceMoved=='wK':
+            self.whiteKingLocation=(move.endRow,move.endCol)
+        elif move.pieceMoved=='bK':
+            self.blackKingLocation=(move.endRow,move.endCol)
 
+        """
+        undo the last move made
+        """
     def undoMove(self):
         if len(self.moveLog)!=0: #make sure that there is a move to undo
             move=self.moveLog.pop()
             self.board[move.startRow][move.startCol]=move.pieceMoved
-            self.board[move.endRow][move.endCol]=move.pieceMoved
+            self.board[move.endRow][move.endCol]=move.pieceCaptured
             self.whiteToMove=not self.whiteToMove  #switch turns back
+            #update the king position if needed
+            if move.pieceMoved == 'wK':
+                self.whiteKingLocation = (move.startRow, move.startCol)
+            elif move.pieceMoved == 'bK':
+                self.blackKingLocation = (move.startRow, move.startCol)
 
     """
     All moves considering checks
     """
     def getValidMoves(self):
-        return self.getAllPossibleMoves()  #for now we will not worry about checks
+        #1.)generate all possible moves
+        moves=self.getAllPossibleMoves()
+        #2.)for each move,make the move
+        for i in range(len(moves)-1,-1,-1):   #when removing from a list go backwards through that list
+            self.makeMove(moves[i])
+            #3.)generate all opponent's moves
+            #4.)for each of your opponent's moves, see if they attack your king
+            self.whiteToMove=not self.whiteToMove
+            if self.inCheck():
+                moves.remove(moves[i]) #5.)if they do attack your king, not a valid move
+            self.whiteToMove=not self.whiteToMove
+            self.undoMove()
+        if len(moves)==0:   #either checkmate or stalemate
+            if self.inCheck():
+                self.checkMate=True
+            else:
+                self.staleMate=True
+        else:
+            self.checkMate=False
+            self.staleMate=False
+
+        return moves
+
+    """
+    Determine if the current player is in check
+    """
+    def inCheck(self):
+        if self.whiteToMove:
+            return self.squareUnderAttack(self.whiteKingLocation[0],self.whiteKingLocation[1])
+        else:
+            return self.squareUnderAttack(self.blackKingLocation[0],self.blackKingLocation[1])
+
+    """
+    Determine if the enemy can attack the square r,c
+    """
+    def squareUnderAttack(self,r,c):
+        self.whiteToMove=not self.whiteToMove  #switch's to opponent's turn
+        oppMoves=self.getAllPossibleMoves()
+        self.whiteToMove=not self.whiteToMove  #switch turns back
+        for move in oppMoves:
+            if move.endRow==r and move.endCol==c:  #square is under Attack
+                return True
+        return False
+
 
     """
     All moves without considering checks
@@ -209,23 +271,3 @@ class Move():
 
     def getRankFile(self,r,c):
         return self.colsToFiles[c]+self.rowsToRanks[r]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
